33bb263413dbca4a9f32d39eaae29ebb86d5171a
Romanio0089 <38210134+Romanio0089@users.noreply.github.com>
2020-06-24 20:46:12 +0200

    Update RMCL-Linux-Installer.sh

--- Linux/RMCL-Linux-Installer.sh
+++ Linux/RMCL-Linux-Installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Can be downloaded from
 #wget http://tiny.cc/RMCLinux && bash RMCLinux
-echo 'Press enter to confirm installation'
+yes Y | command-that-asks-for-imput
 sudo mkdir /RoxMCLauncher
 cd /RoxMCLauncher
 wget http://downtherack.free.fr/RoxMCLauncher/RMCL.jar
