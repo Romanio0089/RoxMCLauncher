79a199fba5bc9b460b54ee2f57f2ceb7f2e6611d
Romanio0089 <38210134+Romanio0089@users.noreply.github.com>
2020-06-24 20:35:39 +0200

    Add files via upload

---
+++ Linux/RMCL-Linux-Installer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Can be downloaded from
+#wget http://tiny.cc/RMCLinux && bash RMCLinux
+sudo mkdir /home/RoxMCLauncher
+cd /home/RoxMCLauncher
+wget https://github.com/Romanio0089/RoxMCLauncher/raw/master/Linux/RMCL.jar
