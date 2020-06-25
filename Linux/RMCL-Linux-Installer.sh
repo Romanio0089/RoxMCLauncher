47a95a28c237894d700d36675b7dc58a04699ac0
Romanio0089 <38210134+Romanio0089@users.noreply.github.com>
2020-06-25 16:47:35 +0200

    Update RMCL-Linux-Installer.sh

--- Linux/RMCL-Linux-Installer.sh
+++ Linux/RMCL-Linux-Installer.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
-#Can be downloaded from
-#wget http://tiny.cc/RMCLinux && bash RMCLinux
-echo "Do you want to continue? (y/n)"
-read input
-if [ "$input" == "yes" "y" "Y" ]
-then
-echo "continue"
-sudo mkdir /RoxMCLauncher
-cd /RoxMCLauncher
-curl 'http://downtherack.free.fr/RoxMCLauncher/RMCL.jar'
-fi
+ 
+read -r -p "Are You Sure? [Y/n] " input
+ 
+case $input in
+    [yY][eE][sS]|[yY])
+ echo "Operation confirmed"
+ ;;
+    [nN][oO]|[nN])
+ echo "Operation cancelled"
+       ;;
+    *)
+ echo "Invalid input..."
+ exit 1
+ ;;
+esac
+
+sudo mkdir /usr/RoxMCLauncher
+cd /usr/RoxMCLauncher
+sudo apt update && sudo apt upgrade
+sudo apt install curl
+sudo curl http://downtherack.free.fr/RoxMCLauncher/RMCL.jar --output RMCL.jar
+sudo curl https://raw.githubusercontent.com/Romanio0089/RoxMCLauncher/master/Linux/RMCLoader.sh --output RMCLoader.sh
+sudo curl https://raw.githubusercontent.com/Romanio0089/RoxMCLauncher/master/Linux/RoxMCLauncher.desktop --output RoxMCLauncher.desktop
+sudo curl http://downtherack.free.fr/RoxMCLauncher/icon.svg --output icon.svg
+sudo chmod a+x RoxMCLauncher.desktop
+sudo cp RoxMCLauncher.desktop ~/.local/share/applications/
+cd ~/.local/share/applications/
+desktop-file-validate RoxMCLauncher.desktop
+sudo update-desktop-database
+echo "RoxMCLauncher has been installed ! To run it, search for RoxMCLauncher in the app library."
